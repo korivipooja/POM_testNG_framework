7f7922a0fd82e7c2223f4cfabcc0f9f8ea350939
Pooja Korivi <korivipooja97@gmail.com>
2026-09-23 01:37:26 +0530

    Initial POM TestNG automation framework

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverManager;
+import utils.configReader;
+
+import java.io.IOException;
+
+public class BaseTest {
+
+    @BeforeMethod
+    public void setUp() throws IOException {
+        configReader.loadProperties();
+        DriverManager.initializeDriver();
+        DriverManager.getDriver()
+                .get(configReader.getProperty("url"));
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        DriverManager.quitDriver();
+    }
+}
